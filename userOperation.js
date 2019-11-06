a8e38b6bd0c1013dd039c52f7fd565f539ca0b19
uday singh <[udayskai@gmail.com]>
2019-11-06 21:32:18 +0530

    Foreach Pending

--- userOperation.js
+++ userOperation.js
@@ -3,17 +3,23 @@
 
     let _arrayData=[];
 
+     this.extra= function (){
+          _arrayData.filter("uday")
+     }
 
+
+     
     this.arrayMethod=function(userModel){
                _arrayData.push(userModel);
     }
 
 
-    this.printMethod= function(userModel){
+    this.printMethod= function(){
     _arrayData
         .forEach((userModel)=>{
             console.log(`${userModelobj1.firstName}${userModelobj1.lastName}`)
         });
     };
  
+  
 };
